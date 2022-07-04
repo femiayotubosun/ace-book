f740432ca3bb4dbf3849f04db8fc6d1563efff68
Femi Ayo-Tubosun <femiayotubosun@gmail.com>
2022-07-04 11:13:21 +0100

    Added postgres

--- book/tests/routers_test.py
+++ book/tests/routers_test.py
@@ -13,19 +13,23 @@ def client():
     yield TestClient(app)
 
 
-def test_get_many_books(client):
+class TestBookRouters:
+
     service_mock = mock.Mock(spec=BookService)
     service_mock.get_many_books.return_value = [
         Book(id=1, title="A Great Book"),
         Book(id=2, title="Another Great Book"),
     ]
 
-    with app.container.book_service.override(service_mock):
-        response = client.get("/books")
+    def test_get_many_books(self, client):
 
-    assert response.status_code == 200
-    data = response.json()
-    assert data == [
-        {"id": 1, "title": "A Great Book"},
-        {"id": 2, "title": "Another Great Book"},
-    ]
+        with app.container.book_service.override(self.service_mock):
+            response = client.get("/books")
+
+        assert response.status_code == 200
+        data = response.json()
+
+        assert data == [
+            {"id": 1, "title": "A Great Book"},
+            {"id": 2, "title": "Another Great Book"},
+        ]
